453949b1c3195ca4afed3ead7e5e57d020f3fbed
Olegborodko <220v2@mail.ru>
2014-12-25 14:19:53 +0200

    model User, controller users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkRails1::Application.config.secret_token = 'e66bd22a81b9fc5b3e1318c546f7e02d5693548f946b7590e53e7cf61a67d315d594b72e3aacca5a186df94e348824109eaa2edf11b603b7815e7a614f685b01'
