8b503138287380c70a6461fc4425cb732e5f0b25
Olegborodko <220v2@mail.ru>
2014-12-28 23:08:49 +0200

    add edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,37 @@ class UsersController < ApplicationController
 
   def index
 
- 
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
 	def new
 		@user = User.new
 	end
 
+  def update
+
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+    if @user.errors.empty?
+      
+     redirect_to root_url
+    else
+      render 'edit'
+    end
+  end
+
   def create
-    @user = User.new(params[:user])
+    @user = User.new(params[:user]) #?
     if @user.save
+
+      session[:user_id] = @user.id
+
       redirect_to root_url, :notice => "Signed up!"
+
+
     else
       render "new"
     end
